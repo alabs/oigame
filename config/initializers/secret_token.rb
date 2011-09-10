0c811c8aa2874afb041cafdce3fdf22b4f2e8e90
Antonio Pardo <apardo@alabs.es>
2011-09-10 05:55:00 +0200

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oigame::Application.config.secret_token = '903a3b16a5bd691c70e6ab5c5ad4ceb33dcba1bc1df496f5538fb67a2dfa4e3ad416b08548630fc36a43fac4a1a6e3341c280e7d6e28d721957f0e13f914cbf9'
