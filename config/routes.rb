de5eb40a12f36976430527947c02da38b49e771a
Antonio Pardo <apardo@alabs.es>
2011-09-10 08:46:39 +0200

    proto navbar y logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Oigame::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +50,7 @@ Oigame::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
