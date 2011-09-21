feaca002088ec27befd8270bf1787e403c34e96d
Antonio Pardo <apardo@alabs.es>
2011-09-21 17:50:48 +0200

    controlador de campañas al estilo normal

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,83 @@
-class CampaignsController < InheritedResources::Base
+class CampaignsController < ApplicationController
+  # GET /campaigns
+  # GET /campaigns.json
+  def index
+    @campaigns = Campaign.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @campaigns }
+    end
+  end
+
+  # GET /campaigns/1
+  # GET /campaigns/1.json
+  def show
+    @campaign = Campaign.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @campaign }
+    end
+  end
+
+  # GET /campaigns/new
+  # GET /campaigns/new.json
+  def new
+    @campaign = Campaign.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @campaign }
+    end
+  end
+
+  # GET /campaigns/1/edit
+  def edit
+    @campaign = Campaign.find(params[:id])
+  end
+
+  # POST /campaigns
+  # POST /campaigns.json
+  def create
+    @campaign = Campaign.new(params[:campaign])
+
+    respond_to do |format|
+      if @campaign.save
+        format.html { redirect_to @campaign, notice: 'Campaign was successfully created.' }
+        format.json { render json: @campaign, status: :created, location: @campaign }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @campaign.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /campaigns/1
+  # PUT /campaigns/1.json
+  def update
+    @campaign = Campaign.find(params[:id])
+
+    respond_to do |format|
+      if @campaign.update_attributes(params[:campaign])
+        format.html { redirect_to @campaign, notice: 'Campaign was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @campaign.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /campaigns/1
+  # DELETE /campaigns/1.json
+  def destroy
+    @campaign = Campaign.find(params[:id])
+    @campaign.destroy
+
+    respond_to do |format|
+      format.html { redirect_to campaigns_url }
+      format.json { head :ok }
+    end
+  end
 end
