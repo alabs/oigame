195fa3fefaf257f53d6aea8184130b14340363fb
Antonio Pardo <apardo@alabs.es>
2011-09-25 17:54:19 +0200

    implementado los tags en campaña

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,10 +2,10 @@ class Campaign < ActiveRecord::Base
 
   belongs_to :user
   
-  attr_accessible :name, :intro, :body
+  attr_accessible :name, :intro, :body, :tag_list
 
   validates_presence_of :name, :intro, :body
   validates_uniqueness_of :name
 
-  acts_as_taggable_on :tags
+  acts_as_taggable
 end
