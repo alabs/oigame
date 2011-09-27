68ec0294783edd0392b6ddb393f22c6e7ef0db48
Antonio Pardo <apardo@alabs.es>
2011-09-27 07:27:37 +0200

    corregido tipo

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,7 +89,7 @@ namespace :deploy do
       ln -s #{shared_path}/system #{latest_release}/public/system &&
       ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
       ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml &&
-      ln -sf #{shared_path}/uploads #{latest_release}/public/uploads
+      ln -sf #{shared_path}/uploads #{latest_release}/public
     CMD
 
     if fetch(:normalize_asset_timestamps, true)
