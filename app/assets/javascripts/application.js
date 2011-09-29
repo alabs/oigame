6b5021c8923e4549c0640919dc22452b6d33af96
Andrés Pereira <apereira@alabs.es>
2011-09-29 08:28:03 +0200

    modal de bienvenida a la beta

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,6 +8,34 @@
 //= require jquery_ujs
 //= require_tree .
 //
+//
+////////////////////////// cookies start
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+////////////////////////// cookies end
 
 ////////////////////////// activity-realtime start
 
@@ -161,5 +189,18 @@ $(function() {
       $('#links-wrapper').offset().top - $('#show-campaign-sidebar').outerHeight() - 10
   });
   // comment-form floating end
+  
+  
+  // modal-beta-home start
+  if ( readCookie('beta-welcome') == null ) {
+    $("#modal-beta-notice").dialog2({
+      removeOnClose: false,
+      autoOpen: true
+    });
+    $("#modal-beta-notice").parent().addClass("modal-large");
+    createCookie('beta-welcome', 'visited', 100); 
+  }
+  // modal-beta-home end
+  
 
 });
