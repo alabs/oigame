86fad719b609db871f364f848cc19d23ed78353c
Antonio Pardo <apardo@alabs.es>
2011-10-10 07:41:10 +0200

    anotación

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,7 @@ class Message < ActiveRecord::Base
   belongs_to :campaign
 
   attr_accessible :campaign, :email
+
+  # generar método que valide que un remitente no puede enviar
+  # más de un mensaje por campaña
 end
