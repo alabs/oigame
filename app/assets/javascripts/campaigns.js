6305d858d5107429beba538b4d5a248897b8fc86
Andrés Pereira <apereira@alabs.es>
2011-10-10 08:19:18 +0200

    campaign: quitando scroll en formulario

--- app/assets/javascripts/campaigns.js
+++ app/assets/javascripts/campaigns.js
@@ -120,16 +120,16 @@ $(function() {
   });
   // modal-widget end
 
-  if ( $("#campaign-actions").length > 0 ) {
-    // comment-form floating start
-    $('#show-campaign-sidebar').scrollToFixed({
-      marginTop:
-        $('#header').outerHeight() + 10,
-      limit:
-        $('#campaign-actions').offset().top - $('#show-campaign-sidebar').outerHeight() - 10
-    });
-    // comment-form floating end
-  }
+//  if ( $("#campaign-actions").length > 0 ) {
+//    // comment-form floating start
+//    $('#show-campaign-sidebar').scrollToFixed({
+//      marginTop:
+//        $('#header').outerHeight() + 10,
+//      limit:
+//        $('#campaign-actions').offset().top - $('#show-campaign-sidebar').outerHeight() - 10
+//    });
+//    // comment-form floating end
+//  }
 
   $(".show_markdown_help").click( function(event){
     event.preventDefault();
