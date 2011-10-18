010f01bfcf1491bff2890d29f3f1815c3b3d2ee0
Antonio Pardo <apardo@alabs.es>
2011-10-19 00:44:20 +0200

    no caché por ahora

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,15 +3,12 @@ class PagesController < ApplicationController
   def index
     @campaigns = Campaign.last_campaigns(3)
     #@users = User.count
-    set_http_cache(3.hours)
   end
 
   def answers
-    set_http_cache(12.hours)
   end
 
   def privacy_policy
-    set_http_cache(24.hours)
   end
   
   def contact
@@ -27,11 +24,9 @@ class PagesController < ApplicationController
       end
     else
       @contact = Contact.new
-      set_http_cache(24.hours)
     end
   end
 
   def contact_received
-    set_http_cache(24.hours)
   end
 end
