df93b89988fadae1158585f00959d24636807e71
Antonio Pardo <apardo@alabs.es>
2011-10-29 20:32:18 +0200

    instalado redis store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,5 @@ Oigame::Application.config.session_store :cookie_store, key: '_oigame_session'
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Oigame::Application.config.session_store :active_record_store
+#Oigame::Application.config.session_store :active_record_store
+Oigame::Application.config.session_store :redis_session_store
