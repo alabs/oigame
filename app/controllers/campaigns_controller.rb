bf4c752b9a4c274b6042b0021337a1823caac140
Andrés Pereira <apereira@alabs.es>
2012-03-16 08:08:44 +0100

    honeypot captcha: #341 - formulario oculto

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 class CampaignsController < ApplicationController
 
+  before_filter :protect_from_spam, :only => [:message, :petition]
   protect_from_forgery :except => [:message, :petition]
   layout :sub_oigame_layout, :except => [:widget, :widget_iframe]
   before_filter :authenticate_user!, :only => [:new, :edit, :create, :update, :destroy, :moderated, :activate]
