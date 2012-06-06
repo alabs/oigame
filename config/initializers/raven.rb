409d191f74c9384572383093f61a274d064c0c6a
Antonio Pardo <apardo@alabs.es>
2012-06-06 09:27:34 +0200

    mejor conf raven

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,11 @@
 require 'raven'
 
 Raven.configure do |config|
-    config.dsn = 'http://b66e78aac5ef4754bba8e04c3d171c56:bad2df3393284b608b41438531c6064a@192.168.163.202:9100/2'
+  if Rails.env == 'development'
+    host = 'sentry.alabs.es'
+  else
+    host = '192.168.163.202'
+  end
+  config.dsn = "http://b66e78aac5ef4754bba8e04c3d171c56:bad2df3393284b608b41438531c6064a@#{host}:9100/2"
+  config.environments = %w[ development staging production ]
 end
