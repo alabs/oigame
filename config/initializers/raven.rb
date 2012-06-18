e8ca10f9a4428c4f5921cae2026382b14e14f231
Andrés Pereira <apereira@alabs.es>
2012-06-18 11:50:57 +0200

    raven testing

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,5 +2,5 @@ require 'raven'
 
 Raven.configure do |config|
   config.dsn = "http://f2f42f62a30946028c62b3b8627738ad:03992acd27ff4c3d99fe5b168e665931@sentry.alabs.es/2"
-  config.environments = %w[ staging production ]
+#  config.environments = %w[ staging production ]
 end
