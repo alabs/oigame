c0f65bd14baaa042fc16475dc4ceb7c489d0add6
Andrés Pereira <apereira@alabs.es>
2012-06-18 11:29:06 +0200

    testing raven

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,7 +1,6 @@
 require 'raven'
 
 Raven.configure do |config|
-  host = '192.168.163.202'
-  config.dsn = "http://b66e78aac5ef4754bba8e04c3d171c56:bad2df3393284b608b41438531c6064a@#{host}:9100/2"
+  config.dsn = "http://b66e78aac5ef4754bba8e04c3d171c56:bad2df3393284b608b41438531c6064a@sentry.alabs.es:9100/2"
   config.environments = %w[ staging production ]
 end
