d146abaae77b7171c15cfa9e1c081313c6e928ca
Antonio Pardo <apardo@alabs.es>
2012-06-21 11:30:52 +0200

    intentando hacer pasar la migración en staging

--- db/migrate/20120621090553_fix_key_length_on_users_authentication_token.rb
+++ db/migrate/20120621090553_fix_key_length_on_users_authentication_token.rb
@@ -1,6 +1,6 @@
 class FixKeyLengthOnUsersAuthenticationToken < ActiveRecord::Migration
   def up
-    remove_index :users, :name => 'index_users_on_authentication_token'
+    remove_index(:users, :name => 'index_users_on_authentication_token') rescue nil
     add_index :users, :authentication_token, :name => 'index_users_on_authentication_token', :unique => true, :length => 254
   end
 
