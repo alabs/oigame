a3361fe702a71d66c8d9272574bb8c40261c6da1
Antonio Pardo <apardo@alabs.es>
2012-06-21 11:26:03 +0200

    intentando hacer pasar la migración en staging

--- db/migrate/20120621090346_fix_key_length_on_users_confirmation_token.rb
+++ db/migrate/20120621090346_fix_key_length_on_users_confirmation_token.rb
@@ -1,6 +1,6 @@
 class FixKeyLengthOnUsersConfirmationToken < ActiveRecord::Migration
   def up
-    remove_index :users, :name => "index_users_on_confirmation_token"
+    remove_index(:users, :name => "index_users_on_confirmation_token") rescue nil
     add_index :users, :confirmation_token, :name => "index_users_on_confirmation_token", :unique => true, :length => 254
   end
 
