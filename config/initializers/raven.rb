58487b7e4849329ebc49da0a56e3776f07d3be90
Andrés Pereira <apereira@alabs.es>
2012-06-27 15:11:29 +0200

    raven funcional

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,5 +2,5 @@ require 'raven'
 
 Raven.configure do |config|
   config.dsn = "http://b66e78aac5ef4754bba8e04c3d171c56:bad2df3393284b608b41438531c6064a@sentry.alabs.es:9100/2"
-  config.environments = %w[ development staging production ]
+  config.environments = %w[ staging production ]
 end
