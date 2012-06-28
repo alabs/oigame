b07f29d01be5b9b83e660a7262669f958c9f40a3
Antonio Pardo <apardo@alabs.es>
2012-06-28 18:01:03 +0200

    cambiado asssets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,17 +9,17 @@ Oigame::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   #config.assets.compile = false
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
