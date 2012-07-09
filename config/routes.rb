aeec0bfafff2541d64b99f33372251b7e2f5f78c
Antonio Pardo <apardo@alabs.es>
2012-07-10 00:10:15 +0200

    active admin instalado

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Oigame::Application.routes.draw do
 
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :sub_oigames, :path => "o" do
     resources :campaigns do
       member do
