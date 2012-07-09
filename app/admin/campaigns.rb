b164a0f33407a92e604855da37f32f7c48f03cea
Antonio Pardo <apardo@alabs.es>
2012-07-10 01:05:24 +0200

    proto admin interface

--- app/admin/campaigns.rb
+++ app/admin/campaigns.rb
@@ -1,3 +1,25 @@
+# encoding: utf-8
 ActiveAdmin.register Campaign do
   
+  menu :label => 'Campañas', :priority => 2
+
+  index :title => 'Campañas' do
+    column :id
+    column :name
+    column :moderated
+    column :ttype
+    column :status
+    column 'Creación', :created_at
+    column 'Publicación', :published_at
+    default_actions
+  end
+
+  filter :name
+  filter :moderated
+  filter :ttype
+  filter :status
+
+  before_filter do
+    @campaign = Campaign.find_by_slug(params[:id])
+  end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register User do
   
+  menu :label => 'Usuarios', :priority => 1
+  
+  index :title => 'Usuarios' do
+    column :id
+    column :email
+    column 'Rol', :role
+    column 'Fecha de registro', :created_at
+    default_actions
+  end
+  
+  filter :email, :label => 'Email'
+  filter :role, :label => 'Rol', :as => :select, :collection => proc { User::ROLES }
 end
