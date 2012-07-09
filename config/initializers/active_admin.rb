c0d2ca93bff91c1d0c9f327ef77c75583edae2f4
Antonio Pardo <apardo@alabs.es>
2012-07-10 00:31:37 +0200

    active admin config

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -10,7 +10,7 @@ ActiveAdmin.setup do |config|
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  # config.site_title_link = "/"
+  config.site_title_link = "/"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
@@ -34,7 +34,7 @@ ActiveAdmin.setup do |config|
   #   config.default_namespace = false
   #
   # Default:
-  # config.default_namespace = :admin
+  config.default_namespace = :manage
   #
   # You can customize the settings for each namespace by using
   # a namespace block. For example, to change the site title
