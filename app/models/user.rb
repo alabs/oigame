5212ba22987b6fc14e36e9a54d621ddee9367f95
Antonio Pardo <apardo@alabs.es>
2012-07-10 07:02:41 +0200

    login con facebook proto

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :mailing, :name, :vat
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :mailing, :name, :vat, :provider, :uid
 
   has_many :campaigns, :dependent => :destroy
   has_and_belongs_to_many :sub_oigames
@@ -18,6 +18,27 @@ class User < ActiveRecord::Base
 
     def recent(limit = nil)
       order('created_at DESC').limit(limit)
+    end
+
+    def find_for_facebook_oauth(auth, signed_in_resource=nil)
+      user = where(:provider => auth.provider, :uid => auth.uid).first
+      unless user
+        user = User.create(name:auth.extra.raw_info.name,
+                           provider:auth.provider,
+                           uid:auth.uid,
+                           email:auth.info.email,
+                           password:Devise.friendly_token[0,20]
+                          )
+      end
+      user
+    end
+
+    def new_with_session(params, session)
+      super.tap do |user|
+        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+          user.email = data["email"] if user.email.blank?
+        end
+      end
     end
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120710000914) do
+ActiveRecord::Schema.define(:version => 20120710040350) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -191,6 +191,8 @@ ActiveRecord::Schema.define(:version => 20120710000914) do
     t.string   "name"
     t.string   "vat"
     t.integer  "campaigns_count",        :default => 0
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true, :length => {"authentication_token"=>254}
