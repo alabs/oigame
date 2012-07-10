f986cd16a539173c47c67422c0c4904429657500
Antonio Pardo <apardo@alabs.es>
2012-07-10 11:11:16 +0200

    active admin desactivado

--- config/initializers/fix_i18n_active_admin.rb
+++
@@ -1,3 +0,0 @@
-I18n.locale = :es # or whatever your default locale is
-I18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml")]
-I18n.reload!
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Oigame::Application.routes.draw do
 
-  ActiveAdmin.routes(self)
-  devise_for :admin_users, ActiveAdmin::Devise.config
+  #ActiveAdmin.routes(self)
+  #devise_for :admin_users, ActiveAdmin::Devise.config
 
   resources :sub_oigames, :path => "o" do
     resources :campaigns do
