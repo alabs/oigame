7013f227c9c4e3c3273023e54aada9f1931ee9b5
Antonio Pardo <apardo@alabs.es>
2012-07-26 16:08:35 +0200

    rollback

--- app/controllers/users/omniauth_callbacks.rb
+++ app/controllers/users/omniauth_callbacks.rb
@@ -3,8 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
 
-    #if @user.persisted?
-    if @user
+    if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       sign_in_and_redirect @user, :event => :authentication
     else
