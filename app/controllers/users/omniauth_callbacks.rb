d67bce503bdc2059790d41ffcdabcf53a9a93451
Antonio Pardo <apardo@alabs.es>
2012-08-23 15:53:25 +0200

    proto login twitter

--- app/controllers/users/omniauth_callbacks.rb
+++ app/controllers/users/omniauth_callbacks.rb
@@ -14,14 +14,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def twitter
     auth = env["omniauth.auth"]
-    logger.debug "DEBUG: " + auth.inspect
-    @user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.new
+
+    # Guardar a un fichero en YAML auth
+    #fh = File.new('/tmp/auth.yml', 'w')
+    #fh.puts auth.to_yaml
+    #fh.close
+
+    @user = User.find_for_twitter_oauth(auth, current_user)
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Twitter"
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.twitter_uid"] = auth["uid"]
-      redirect_to new_user_registration_url
+      redirect_to new_user_registration_url(@user) 
     end
   end
 
