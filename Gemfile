0f6121022fd190000c82fd3b0598b33c65ce1299
Andrés Pereira <apereira@alabs.es>
2012-08-31 13:30:18 +0200

    traducciones - tolk - #47

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ gem 'rails_admin'
 gem 'mailcatcher'
 
 gem 'geocoder'
+
+gem 'tolk'
---
+++ config/initializers/tolk.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+# Tolk config file. Generated on August 29, 2012 16:11
+# See github.com/tolk/tolk for more informations
+
+Tolk.config do |config|
+
+  # If you need to add a mapping do it like this :
+  # May we suggest you use http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+  config.mapping["gl"] = 'Galician'
+  config.mapping["eu"] = 'Basque'
+
+end
