7ed0fba3d1c32a2ec60af61e4734eae9419e296f
Andrés Pereira <apereira@alabs.es>
2012-09-06 12:01:29 +0200

    quitando prueba de assets

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -105,8 +105,19 @@ namespace :deploy do
     end
 
     # precompile assets
-    #run "cd #{latest_release}; RAILS_ENV=staging bundle exec rake assets:precompile"
+    run "cd #{latest_release}; RAILS_ENV=staging bundle exec rake assets:precompile"
   end
+
+  #namespace :assets do
+  #  task :precompile, :roles => :web, :except => { :no_release => true } do
+  #    from = source.next_revision(current_revision)
+  #    if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+  #      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+  #    else
+  #      logger.info "Skipping asset pre-compilation because there were no asset changes"
+  #    end
+  #  end
+  #end
 
   desc "Zero-downtime restart of Unicorn"
   task :restart, :except => { :no_release => true } do
@@ -158,20 +169,4 @@ namespace :sphinx do
 end
 
 after 'deploy:finalize_update', 'sphinx:symlink_indexes'
-
-
-
-namespace :deploy do
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-  end
-end
-
 
