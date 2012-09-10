ff2daacdf9e9aa8e2ce03ef617252aa9990e06d8
Antonio Pardo <apardo@alabs.es>
2012-09-10 17:14:36 +0200

    authz

--- app/abilities/guests.rb
+++ app/abilities/guests.rb
@@ -25,15 +25,35 @@ Canard::Abilities.for(:guest) do
   cannot :access, :rails_admin   # revoke access to rails_admin
   cannot :dashboard              # revoke access to the dashboard
   can :read, Campaign, :moderated => false
+  can :read, Campaign do |campaign|
+    unless campaign.sub_oigame.nil?
+      campaign.sub_oigame.users.include? user
+    end
+  end
   can :read, Campaign, :status => 'archived'
+  can :create, Campaign
+  can :update, Campaign, :moderated => false, :user_id => user.id
+  can :participants, Campaign, :moderated => false, :user_id => user.id
   can :widget, Campaign, :moderated => false 
   can :widget_iframe, Campaign, :moderated => false
-  can :message, Campaign, :moderated => false
   can :petition, Campaign, :moderated => false
   can :validate, Campaign, :moderated => false
   can :validated, Campaign, :moderated => false
   can :archived, Campaign, :status => 'archived'
+  can :moderated, Campaign do |campaign|
+    unless campaign.sub_oigame.nil?
+      campaign.sub_oigame.users.include? user
+    end
+  end
+  can :manage, Campaign do |campaign|
+    unless campaign.sub_oigame.nil?
+      campaign.sub_oigame.users.include? user
+    end
+  end
   # sub_oigames
   cannot :read, SubOigame
+  can :manage, SubOigame do |sub|
+    sub.users.include? user
+  end
   cannot :index, SubOigame
 end
