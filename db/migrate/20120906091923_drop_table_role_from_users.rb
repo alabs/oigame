1c4228588ee7140dcb10a0d755526af714902f0d
Antonio Pardo <apardo@alabs.es>
2012-09-10 10:55:09 +0200

    arreglar roles

--- db/migrate/20120906091923_drop_table_role_from_users.rb
+++ db/migrate/20120906091923_drop_table_role_from_users.rb
@@ -1,5 +1,27 @@
 class DropTableRoleFromUsers < ActiveRecord::Migration
-  def change
+  def self.up
+    users = User.where(:role => 'user').all
+    users.each do |user|
+      user.roles = %w[user]
+      user.save
+    end
+
+    editors = User.where(:role => 'editor').all
+    editors.each do |editor|
+      editor.roles = %w[editor]
+      editor.save
+    end
+
+    admins = User.where(:role => 'admin').all
+    admins.each do |admin|
+      admin.roles = %w[admin]
+      admin.save
+    end
+
     remove_column :users, :role
+  end
+
+  def self.down
+    add_column :users, :role, :string, :default => 'user'
   end
 end
