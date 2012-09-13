5b2a4d2afd2a639abdafbfe6cca5be63e342d5ad
Antonio Pardo <apardo@alabs.es>
2012-09-13 23:52:00 +0200

    fix boot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,8 +52,9 @@ class ApplicationController < ActionController::Base
   end
 end
 
-class Devise::Mailer
-  def default_url_options(options={})
-    { :locale => I18n.locale }
-  end
-end
+# Esto peta
+#class Devise::Mailer
+#  def default_url_options(options={})
+#    { :locale => I18n.locale }
+#  end
+#end
