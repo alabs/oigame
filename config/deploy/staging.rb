8f59c56af0277ad67de25bf237d2509606f5ae8f
Antonio Pardo <apardo@alabs.es>
2012-09-14 15:05:02 +0200

    assets auto

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -120,6 +120,30 @@ namespace :deploy do
   #    end
   #  end
   #end
+  
+  namespace :assets do
+
+    task :precompile, :roles => :web do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+        run_locally("rake assets:clean && rake assets:precompile")
+        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+        run_locally "rm public/assets.tar.bz2"
+        run_locally("rake assets:clean")
+      else
+        logger.info "Skipping asset precompilation because there were no asset changes"
+      end
+    end
+
+    task :symlink, roles: :web do
+      run ("rm -rf #{latest_release}/public/assets &&
+            mkdir -p #{latest_release}/public &&
+            mkdir -p #{shared_path}/assets &&
+            ln -s #{shared_path}/assets #{latest_release}/public/assets")
+    end
+  end
 
   desc "Restart the Thin processes"
   task :restart do
@@ -178,3 +202,5 @@ end
 
 after 'deploy:finalize_update', 'sphinx:symlink_indexes'
 
+before 'deploy:finalize_update', 'deploy:assets:symlink'
+after 'deploy:update_code', 'deploy:assets:precompile'
