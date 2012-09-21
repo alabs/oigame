7377495b104aae96ac65751456c6467f199e0ea6
Antonio Pardo <apardo@alabs.es>
2012-09-21 15:49:35 +0200

    validaciones y callbacks

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,4 +2,17 @@ class Donation < ActiveRecord::Base
   
   belongs_to :campaign
   belongs_to :user
+
+  validates_presence_of :campaign_id
+  validates_presence_of :amount
+
+  after_create :add_credit_to_campaign
+
+  protected
+
+  def add_credit_to_campaign
+    campaign = self.campaign
+    campaign.credit += self.amount
+    campaign.save
+  end
 end
