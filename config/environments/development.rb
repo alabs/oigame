e282bdaed35c53424890769a8b3fb4a0ce496b7d
Antonio Pardo <apardo@alabs.es>
2012-11-28 00:22:41 +0100

    solucionado problema que no compilaba el fichero sass

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,6 +22,8 @@ Oigame::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
+  config.assets.precompile += ['responsive.css']
+
   # Do not compress assets
   config.assets.compress = false
 
