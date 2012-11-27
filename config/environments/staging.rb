41ac5d4173b62f66e1a6236e96a2ff44066d88b4
Antonio Pardo <apardo@alabs.es>
2012-11-28 00:27:54 +0100

    a ver si lo carga desde el entorno

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,6 +11,8 @@ Oigame::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  config.assets.precompile += ['responsive.css']
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
