9928909734455dc230e6e84cc123267d822ce938
Antonio Pardo <apardo@alabs.es>
2013-01-06 16:21:11 +0100

    limpiando

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,4 +1,4 @@
-FACEBOOK_SCOPE = 'user_likes,user_photos,user_photo_video_tags'
+FACEBOOK_SCOPE = 'user_likes,publish_stream,publish_actions'
 
 unless APP_CONFIG[:FACEBOOK_APP_ID] && APP_CONFIG[:FACEBOOK_SECRET]
   abort("missing env vars: please set FACEBOOK_APP_ID and FACEBOOK_SECRET with your app credentials")
