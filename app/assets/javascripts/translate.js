bffd7167462ecc55630faa8e2c0446d2d5e32b05
Andrés Pereira <apereira@alabs.es>
2013-01-10 11:35:12 -0200

    tolk: i18n busqueda a traves de http://mymemory.translated.net/api

---
+++ app/assets/javascripts/translate.js
@@ -0,0 +1,28 @@
+
+$.noConflict();
+
+jQuery.fn.directText = function(settings) {
+   settings = jQuery.extend({},jQuery.fn.directText.defaults, settings);
+   return this.contents().map(function() {
+     if (this.nodeType != 3) return undefined; // remove non-text nodes
+     var value = this.nodeValue;
+     if (settings.trim) value = jQuery.trim(value);
+     if (!value.length) return undefined; // remove zero-length text nodes
+     return value;
+   }).get().join(settings.joinText);
+};
+
+jQuery.fn.directText.defaults = {
+   trim: true,
+   joinText: ''
+};
+
+jQuery('.search_translate').click(function(e) {
+  e.preventDefault();
+  var that = jQuery(this);
+  var phrase = that.parent().parent().children('.phrase').directText();
+  jQuery.getJSON('http://mymemory.translated.net/api/get?q=' + phrase + '!&langpair=es|en', function(data){
+    that.parent().parent().children('.translation').children('textarea').val(data.responseData.translatedText);
+  });
+});
+
