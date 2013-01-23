2e86baea4140782849b52fff6f722f939171f012
Andrés Pereira <apereira@alabs.es>
2013-01-23 12:05:53 -0300

    tolk: assets: arreglando deploy en staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,7 +11,7 @@ Oigame::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
 
-  config.assets.precompile += ['responsive.css']
+  config.assets.precompile += ['responsive.css', 'translate.css', 'translate.js']
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
