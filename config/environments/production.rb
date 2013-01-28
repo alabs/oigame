7bae6b85903f88d15bbfaf6509bb70168c32a936
Antonio Pardo Sánchez <apardo@alabs.org>
2013-01-28 08:03:26 +0100

    different errbit app for staging and producition

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,3 +88,10 @@ Oigame::Application.configure do
   # SSL en todo el sitio
   config.force_ssl = true
 end
+
+Airbrake.configure do |config|
+  config.api_key = 'c6626f26039ab33d1b509b50bde13659'
+  config.host    = 'err.oiga.me'
+  config.port    = 443
+  config.secure  = config.port == 443
+end
