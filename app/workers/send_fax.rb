cc4ef70b897c96d695d244b7097f2efe30328dc0
Antonio Pardo Sánchez <apardo@alabs.org>
2013-01-28 04:27:28 +0100

    remove credit from campaign

--- app/workers/send_fax.rb
+++ app/workers/send_fax.rb
@@ -4,6 +4,7 @@ class SendFax
 
   def self.perform fax_id
     campaign = Fax.find(fax_id).campaign
+    campaign.credit -= (FaxForRails::TAX * campaign.numbers.size)
     Mailman.send_message_to_fax_recipients(fax_id, campaign.id).deliver
   end
 end
