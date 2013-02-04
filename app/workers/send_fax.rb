479968a051dd88addfe1cf0d4f31c93adce3033b
Antonio Pardo Sánchez <apardo@alabs.org>
2013-02-04 10:52:39 -0500

    more clear

--- app/workers/send_fax.rb
+++ app/workers/send_fax.rb
@@ -4,8 +4,7 @@ class SendFax
 
   def self.perform fax_id
     campaign = Fax.find(fax_id).campaign
-    credits = campaign.numbers.size
-    campaign.credit = campaign.credit - credits
+    campaign.credit -= campaign.numbers.size
     campaign.save
     Mailman.send_message_to_fax_recipients(fax_id, campaign.id).deliver
   end
