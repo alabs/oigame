6aa750ceb123db3d222267fc07fac98713fe1e29
Antonio Pardo Sánchez <apardo@alabs.org>
2013-02-04 19:35:49 -0500

    ending to fix credits restore

--- app/workers/send_fax.rb
+++ app/workers/send_fax.rb
@@ -4,12 +4,10 @@ class SendFax
 
   def self.perform fax_id
     campaign = Fax.find(fax_id).campaign
-    campaign.numbers.each do |number|
-      while campaign.has_credit?
-        Mailman.send_message_to_fax_recipient(fax_id, campaign.id, number).deliver
-        campaign.credit = campaign.credit - 1
-        campaign.save
-      end
+    while campaign.has_credit?
+      Mailman.send_message_to_fax_recipients(fax_id, campaign.id).deliver
+      campaign.credit -= campaign.numbers.size
+      campaign.save
     end
   end
 end
