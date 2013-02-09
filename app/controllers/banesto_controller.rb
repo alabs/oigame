13ee2f12f8ddc582bea8434e4b0ed47685f231b7
Antonio Pardo Sánchez <apardo@alabs.org>
2013-02-09 13:21:56 -0500

    no basic auth, protect with ip

--- app/controllers/banesto_controller.rb
+++ app/controllers/banesto_controller.rb
@@ -1,12 +1,25 @@
 class BanestoController < ApplicationController
 
-  http_basic_authenticate_with :name => "gateway", :password => APP_CONFIG[:gw_pass]
-  
+  before_filter :protect_payment_accepted
+
   skip_before_filter :verify_authenticity_token
 
   def payment_accepted
     campaign = Campaign.find_by_slug(params[:id])
     campaign.credit += (params[:amount] / FaxForRails::TAX)
     campaign.save
+  end
+
+  protected
+
+  def protect_payment_accepted
+    ips = ['94.23.203.58']
+    unless ips.include? request.remote_ip
+      # Check for your subnet stuff here, for example
+      # if not request.remote_ip.include?('127.0,0')
+      render :text => "You are unauthorized"
+
+      return
+    end
   end
 end
