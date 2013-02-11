23f23b015dbacfebb6535a941e8d023cef355e57
Antonio Pardo Sánchez <apardo@alabs.org>
2013-02-11 07:07:53 -0500

    more clear

--- app/controllers/banesto_controller.rb
+++ app/controllers/banesto_controller.rb
@@ -6,7 +6,7 @@ class BanestoController < ApplicationController
 
   def payment_accepted
     campaign = Campaign.find_by_slug(params[:id])
-    campaign.credit += (params[:amount].to_i / FaxForRails::TAX)
+    campaign.credit += params[:amount].to_i / FaxForRails::TAX
     campaign.save
 
     render :nothing => true
