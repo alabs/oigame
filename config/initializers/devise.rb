04a300f56d8ebc39692d114ef235c513730021f6
Enrique Alvarez <enriqueaf@gmail.com>
2013-02-15 19:47:52 +0100

    FB permissions

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -206,7 +206,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   require 'omniauth-facebook'
   require "#{Rails.root.to_s}/app/controllers/users/omniauth_callbacks"
-  config.omniauth :facebook, APP_CONFIG[:FACEBOOK_APP_ID], APP_CONFIG[:FACEBOOK_SECRET]
+  config.omniauth :facebook, APP_CONFIG[:FACEBOOK_APP_ID], APP_CONFIG[:FACEBOOK_SECRET], scope: 'user_likes,publish_stream,publish_actions'
   
   #require 'omniauth-twitter'
   #config.omniauth :twitter, APP_CONFIG[:twitter_consumer_key], APP_CONFIG[:twitter_consumer_secret]
