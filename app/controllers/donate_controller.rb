edea34f43f8e945931c5e35002e3326729432690
Antonio Pardo Sánchez <apardo@alabs.org>
2013-04-04 11:13:52 -0500

    working in simple donation

--- app/controllers/donate_controller.rb
+++ app/controllers/donate_controller.rb
@@ -16,17 +16,16 @@ class DonateController < ApplicationController
     end
 
     @reference = secure_digest(Time.now, (1..10).map { rand.to_s})[0,29]
+    @coste = params[:coste]
 
     @data = {}
     @data[:reference] = @reference
     @data[:name] = params[:name]
     @data[:idcard] = params[:idcard]
     @data[:email] = params[:email]
-    @coste = params[:coste]
     HTTParty.post("http://#{APP_CONFIG[:gw_domain]}/pre", :body => @data)
 
   end
-
   def accepted
   end
 
