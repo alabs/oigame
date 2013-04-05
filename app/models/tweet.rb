6e5e6808bcf1cb0608e8dd32d567bf6b1bf2c1b8
Antonio Pardo Sánchez <apardo@alabs.org>
2013-04-05 14:51:49 -0500

    msgs

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,6 @@ class Tweet < ActiveRecord::Base
   attr_accessible :msg
 
   def self.last_messages(section)
-    self.order('created_at DESC').limit(2)
+    order('created_at DESC').limit(2)
   end
 end
