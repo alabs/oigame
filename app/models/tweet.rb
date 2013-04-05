ff30bab50a5b9ed8795bce485a3454fe4d4b6912
Antonio Pardo Sánchez <apardo@alabs.org>
2013-04-05 18:41:21 -0500

    tipo

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,9 +6,9 @@ class Tweet < ActiveRecord::Base
     order('created_at DESC').limit(1).all
   end
 
-  #def text=(args)
-  #  self.msg = convert_tweet_to_html(args)
-  #end
+  def text=(args)
+    self.msg = args
+  end
 
   #def convert_tweet_to_html(string)
   #  string = string
