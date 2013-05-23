1c93d43b41c2fde3dce74bb707a3582b2f6fa8df
Antonio Pardo <apardo@alabs.org>
2013-05-23 09:45:18 -0500

    revert

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Oigame::Application.config.secret_token = ENV['SECRET_TOKEN_PROD']
+Oigame::Application.config.secret_token = '903a3b16a5bd691c70e6ab5c5ad4ceb33dcba1bc1df496f5538fb67a2dfa4e3ad416b08548630fc36a43fac4a1a6e3341c280e7d6e28d721957f0e13f914cbf9'
