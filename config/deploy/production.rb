ec48b83279cd952e9178905c060db83457cda5a2
Andrés Pereira <apereira@alabs.es>
2014-01-30 12:20:21 +0100

    [deploy] thin fix config and deploy missing &&

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -106,7 +106,7 @@ namespace :deploy do
       ln -sf #{shared_path}/config/newrelic.yml #{latest_release}/config/newrelic.yml &&
       ln -sf #{shared_path}/public/sitemap1.xml.gz #{latest_release}/public/sitemap1.xml.gz &&
       ln -sf #{shared_path}/public/sitemap_index.xml.gz #{latest_release}/public/sitemap_index.xml.gz &&
-      ln -sf #{shared_path}/config/recaptcha.rb #{latest_release}/config/initializers/recaptcha.rb
+      ln -sf #{shared_path}/config/recaptcha.rb #{latest_release}/config/initializers/recaptcha.rb &&
       ln -sf #{shared_path}/config/airbrake.rb #{latest_release}/config/initializers/airbrake.rb
     CMD
 
